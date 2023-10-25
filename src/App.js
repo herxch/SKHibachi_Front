1cf37fb13e561927baa2fe8668d6d37ca2ddabbf
herxch <herxch@hotmail.com>
2023-10-24 23:11:18 -0500

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import "./App.css";
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <NavBar />
       <video
         autoPlay
         loop
         muted
         style={{
           // position: "fixed",
-          width: "100%",
+          width: '100%',
           // left: 0,
           // top: 0,
         }}
       >
-        <source src="/VideoHeader.mp4" type="video/mp4" />
+        <source src='/VideoHeader.mp4' type='video/mp4' />
         Your browser does not support the video tag.
       </video>
     </div>
