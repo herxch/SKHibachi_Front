b4516b4c0a40b54594979f9a30805cc146ce6981
herxch <herxch@hotmail.com>
2023-10-30 14:37:42 -0500

    photo

--- src/components/BookingFormContainer.js
+++ src/components/BookingFormContainer.js
@@ -32,11 +32,12 @@ const BookingFormContainer = () => {
           <h2 className={styles.haveFunWith}>skhibachi@gmail.com</h2>
         </div>
         <div className={styles.photoseparateline} />
-        <img
-          className={styles.photoWrapperIcon}
-          alt=""
-          src="/photowrapper@2x.png"
-        />
+        <div className={styles.photoWrapperIcon}>
+          <img className={styles.photo} alt="" src="/photo1.png" />
+          <img className={styles.photo} alt="" src="/photo2.png" />
+          <img className={styles.photo} alt="" src="/photo3.png" />
+          <img className={styles.photo} alt="" src="/photo4.png" />
+        </div>
       </section>
       {isBookTablePopupOpen && (
         <PortalPopup
