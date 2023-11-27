76046c9dabf4c2e227b508bb08e3b116945c9638
herxch <herxch@hotmail.com>
2023-11-27 11:45:54 -0600

    update social media link

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,7 +1,7 @@
 // SocialMedia.jsx
-import React from 'react';
-import styles from './SocialMedia.module.css'; // 使用模块化的 CSS
-import { Link } from 'react-router-dom';
+import React from "react";
+import styles from "./SocialMedia.module.css"; // 使用模块化的 CSS
+import { Link } from "react-router-dom";
 
 const SocialMedia = () => {
   return (
@@ -12,35 +12,35 @@ const SocialMedia = () => {
         </h2>
         <div className={styles.socialMediaIcon}>
           <a
-            href='https://www.google.com'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://www.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
             className={styles.fb}
           ></a>
           <a
-            href='https://www.google.com'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://instagram.com/skhibachi?igshid=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr"
+            target="_blank"
+            rel="noopener noreferrer"
             className={styles.ig}
           ></a>
           <a
-            href='https://www.google.com'
-            target='_blank'
-            rel='noopener noreferrer'
-            className={styles.tw}
+            href="https://www.tiktok.com/@skhibachi?_t=8hhbyZLPeix&_r=1"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.tt}
           ></a>
-          <a
-            href='https://www.google.com'
-            target='_blank'
-            rel='noopener noreferrer'
+          {/* <a
+            href="https://www.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
             className={styles.yt}
           ></a>
           <a
-            href='https://www.google.com'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://www.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
             className={styles.yp}
-          ></a>
+          ></a> */}
         </div>
       </div>
     </div>
